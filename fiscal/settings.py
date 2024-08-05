d15f6c3c4dbb933505e96b74d2ff6a3c1acd5c26
unknown <eukoh@quaad.net>
2024-08-05 12:11:20 -0500

    ...

--- fiscal/settings.py
+++ fiscal/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -157,3 +157,5 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+WHITENOISE_KEEP_ONLY_HASHED_FILES = True
