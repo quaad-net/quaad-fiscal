f3d7504f20eec76af264dbf7694d4141db6ce16e
unknown <eukoh@quaad.net>
2024-08-07 10:10:33 -0500

    ...

--- fiscal/settings.py
+++ fiscal/settings.py
@@ -44,10 +44,11 @@ if not IS_HEROKU_APP:
 
 ALLOWED_HOSTS = ["*"]
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    # Use WhiteNoise's runserver implementation instead of the Django default, for dev-prod parity.
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
